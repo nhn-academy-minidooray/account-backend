a677e600956a34acf5e05df4d36b5de8bfcf4730
dduneon <dduneon@gmail.com>
2024-01-25 00:02:45 +0900

    [INIT] INIT PROJECT

---
+++ src/main/java/com/nhnacademy/minidooray/account/backend/MinidoorayAccountBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.account.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidoorayAccountBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidoorayAccountBackendApplication.class, args);
+	}
+
+}
