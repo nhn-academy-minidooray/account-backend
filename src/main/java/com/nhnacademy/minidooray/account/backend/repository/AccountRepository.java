2019f0d741d8ce819cc59d152ba24e2aaa8a57e2
devhomh <jinwoo7654@naver.com>
2024-01-27 18:23:56 +0900

    [ADD] AccountRestControllerTest

--- src/main/java/com/nhnacademy/minidooray/account/backend/repository/AccountRepository.java
+++ src/main/java/com/nhnacademy/minidooray/account/backend/repository/AccountRepository.java
@@ -5,8 +5,6 @@ import com.nhnacademy.minidooray.account.backend.domain.dto.AccountStatusInfoDTO
 import com.nhnacademy.minidooray.account.backend.entity.Account;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
     Optional<AccountPageInfoDTO> getAccountPageInfoById(String id);
